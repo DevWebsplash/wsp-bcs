ef6af9b4f072da9d44b6a5b0a4fb911d5aeab010
Dev_WSP <dev.wspro@gmail.com>
2024-08-19 20:44:01 +0300

    vehicles - markup

--- twentynineteen/assets/js/scripts/common.js
+++ twentynineteen/assets/js/scripts/common.js
@@ -1,5 +1,32 @@
 jQuery(function($) {
     'use strict';
 
+    //select
+    $('.custom-select select').SumoSelect({
+        search: true,
+        searchText: 'Enter here...',
+        forceCustomRendering: true
+    });
 
+    //slider
+    const swiper1 = new Swiper('.testimonials-slider', {
+        slidesPerView: 1,
+        spaceBetween: 35,
+        navigation: {
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            991: {
+                slidesPerView: 3,
+            },
+        },
+    });
 });
--- twentynineteen/test-template.php
+++ twentynineteen/test-template.php
@@ -4,7 +4,14 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/bcs.min.css">
 <!-- HTML Content here  -->
 
-
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A cupiditate distinctio, ipsum laboriosam laborum,
+  mollitia necessitatibus odit, officiis optio placeat porro praesentium sit vitae. Cupiditate esse omnis ratione
+  tempore. Quos.
+</div>
+<div>Alias et expedita iusto. Eaque laborum magni provident quis temporibus voluptatibus? Ad at dolorum esse expedita
+  ipsa itaque molestias nobis nostrum perferendis perspiciatis provident recusandae, saepe suscipit tempora, unde
+  voluptatibus.
+</div>
 
 
 <?php get_footer(); ?>
