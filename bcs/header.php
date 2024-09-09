a0379c300d150156499d51c02449dc8ff8d2c3ac
Dev_WSP <dev@websplash.pro>
2024-09-09 21:03:24 +0300

    Woo layout functions

--- bcs/header.php
+++ bcs/header.php
@@ -47,7 +47,7 @@
       <div class="header__inner">
         <div class="toggle-menu"><span></span><span></span></div>
         <div class="header__logo">
-          <a href="/" title="Home page"><img src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" loading="lazy" alt=""></a>
+          <a href="/staging/" title="Home page"><img src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" loading="lazy" alt=""></a>
         </div>
         <nav class="header__nav">
           <div class="cn">
@@ -68,7 +68,7 @@
                 </ul>
               </li>
               <li>
-                <a href="#">Products</a>
+                <a href="/staging/products/">Products</a>
                 <span class="icon">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M6.70711 8.29289C6.31658 7.90237 5.68342 7.90237 5.29289 8.29289C4.90237 8.68342 4.90237 9.31658 5.29289 9.70711L11.2929 15.7071C11.6834 16.0976 12.3166 16.0976 12.7071 15.7071L18.7071 9.70711C19.0976 9.31658 19.0976 8.68342 18.7071 8.29289C18.3166 7.90237 17.6834 7.90237 17.2929 8.29289L12 13.5858L6.70711 8.29289Z"/>
@@ -80,8 +80,8 @@
                   <li><a href="#">Headline 3</a></li>
                 </ul>
               </li>
-              <li><a href="portfolio.html">Portfolio</a></li>
-              <li><a href="#">Vehicles</a></li>
+              <li><a href="/staging/">Portfolio</a></li>
+              <li><a href="/staging/vehicle/">Vehicles</a></li>
               <li>
                 <a href="#">About us</a>
                 <span class="icon">
