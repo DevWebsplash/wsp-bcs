0426e9f759f0c5aa7634b7f69fa3bfb733db6d42
Dev_WSP <dev.wspro@gmail.com>
2024-09-19 15:55:40 +0300

    Update woo-global.php

--- bcs/functions/woocommerce/woo-global.php
+++ bcs/functions/woocommerce/woo-global.php
@@ -15,7 +15,7 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 function woo_custom_wrapper_start() {
   $sidebar = get_sidebar();
   $has_sidebar = is_active_sidebar( 'shop-sidebar' );
-  var_dump ($has_sidebar, $sidebar);
+//  var_dump ($has_sidebar, $sidebar);
   if ( $has_sidebar ) {
     echo '<div class="cn cn-sidebar"><div id="primary" class="content-area"><main id="main" class="site-main" role="main">';
   } else {
