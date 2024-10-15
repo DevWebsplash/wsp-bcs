044d801a95fd38b7999a7ae22a56a751fc21fe07
Dev_WSP <dev.wspro@gmail.com>
2024-10-15 18:33:16 +0300

    Cart, checkout, thank you pages styling

--- bcs/page.php
+++ bcs/page.php
@@ -13,13 +13,15 @@
 get_header();
 if ( is_woocommerce() || is_cart() || is_checkout() || is_checkout_pay_page() ) {
 ?>
-	<div class="block-woocommerce" >
-    <header>
+	<div class="block-woocommerce">
+      <header>
+        <div class="cn">
+          <h1 class="block__title"><?php the_title(); ?></h1>
+        </div>
+      </header>
       <div class="cn">
-        <h1 class="block__title"><?php the_title(); ?></h1>
+        <?php the_content(); ?>
       </div>
-    </header>
-    <?php the_content(); ?>
 	</div>
 <?php
 } else if ( is_account_page() ) {
