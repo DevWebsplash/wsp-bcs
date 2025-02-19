7fcb10cb2d0f506247dc499c27161e2b07edff9c
Dev_WSP <dev.wspro@gmail.com>
2025-02-19 19:54:32 +0200

    Fix AirTable

--- bcs/functions/airtable/includes/air-connections.php
+++ bcs/functions/airtable/includes/air-connections.php
@@ -7,6 +7,11 @@
 if (!defined('ABSPATH')) {
   exit;
 }
+
+// wp-config.php
+define('AIRTABLE_PAT', 'patkpWz5coirjheoV.a8dc92e7f906af8d8ad3fa06671fa176cbe48662930c73f9cae52f1c1aea8aab');
+define('AIRTABLE_BASE_ID', 'appu3QXHr7ai2NLwi');
+define('AIRTABLE_TABLE_NAME', 'tblXbISQ9nRhz0YfJ');
 // Recommended: Store sensitive data in environment variables or constants
 // Define constants in `wp-config.php`
 // define('AIRTABLE_PAT', 'your_airtable_pat');
@@ -14,9 +19,9 @@ if (!defined('ABSPATH')) {
 
 
 // Replace with your actual token, base ID, and table name
-$pat       = 'patkpWz5coirjheoV.a8dc92e7f906af8d8ad3fa06671fa176cbe48662930c73f9cae52f1c1aea8aab'; // Airtable Personal Access Token
-$baseId    = 'appu3QXHr7ai2NLwi'; // Airtable Base ID
-$tableName = 'tblXbISQ9nRhz0YfJ'; // Airtable Table Name
+$pat       = AIRTABLE_PAT; // Airtable Personal Access Token
+$baseId    = AIRTABLE_BASE_ID; // Airtable Base ID
+$tableName = AIRTABLE_TABLE_NAME; // Airtable Table Name
 //$tableName = urlencode('Vehicle List Incl. Variant');
 
 $postType  = 'vehicle'; // WordPress Post Type
