e48293c6adcf3af0003aacd85e2e0978b2c9655c
Dev_WSP <dev.wspro@gmail.com>
2025-02-21 12:17:28 +0200

    AirTable MODELS Meta

--- bcs/functions/airtable/includes/air-connections.php
+++ bcs/functions/airtable/includes/air-connections.php
@@ -12,6 +12,8 @@ if (!defined('ABSPATH')) {
 define('AIRTABLE_PAT', 'patkpWz5coirjheoV.a8dc92e7f906af8d8ad3fa06671fa176cbe48662930c73f9cae52f1c1aea8aab');
 define('AIRTABLE_BASE_ID', 'appu3QXHr7ai2NLwi');
 define('AIRTABLE_TABLE_NAME', 'tblXbISQ9nRhz0YfJ');
+
+define('AIRTABLE_TABLE_MODELS_Meta', 'tblk2WqLZS5wdPPqp');
 // Recommended: Store sensitive data in environment variables or constants
 // Define constants in `wp-config.php`
 // define('AIRTABLE_PAT', 'your_airtable_pat');
@@ -22,6 +24,8 @@ define('AIRTABLE_TABLE_NAME', 'tblXbISQ9nRhz0YfJ');
 $pat       = AIRTABLE_PAT; // Airtable Personal Access Token
 $baseId    = AIRTABLE_BASE_ID; // Airtable Base ID
 $tableName = AIRTABLE_TABLE_NAME; // Airtable Table Name
+
+$tableModels_Meta = AIRTABLE_TABLE_NAME; // Airtable Table MODELS Meta
 //$tableName = urlencode('Vehicle List Incl. Variant');
 
 $postType  = 'vehicle'; // WordPress Post Type
