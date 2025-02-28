f3b777d4afed6fd8b82b6bc5f85833b44b342ffe
Dev_WSP <screwjke@gmail.com>
2025-02-28 16:59:37 +0200

    Update func-taxonomies.php

--- bcs/functions/func-taxonomies.php
+++ bcs/functions/func-taxonomies.php
@@ -6,123 +6,94 @@
 /**
  * Register Custom Post Type vehicle
  */
-// Реєстрація Vehicle CPT
 function register_vehicle_cpt() {
-	$labels = array(
-		'name' => 'Vehicles',
-		'singular_name' => 'Vehicle',
-	);
-
 	$args = array(
 		'label' => 'Vehicle',
 		'public' => true,
-		'has_archive' => true,
+		'has_archive' => 'vehicle',
 		'rewrite' => array('slug' => 'vehicle', 'with_front' => false),
 		'supports' => array('title', 'editor', 'custom-fields'),
 	);
-
 	register_post_type('vehicle', $args);
 }
 add_action('init', 'register_vehicle_cpt');
 
-function add_vehicle_archive_body_class($classes) {
-  if (is_post_type_archive('vehicle') || is_singular('vehicle') || (is_tax('make') && is_tax('make', array('vehicle')))) {
-    $classes[] = 'vehicle-archive';
-  } elseif (is_tax('make')) {
-    $queried_object = get_queried_object();
-    if ($queried_object && isset($queried_object->taxonomy) && $queried_object->taxonomy === 'make') {
-      $post_types = get_taxonomy('make')->object_type;
-      if (in_array('vehicle', $post_types)) {
-        $classes[] = 'vehicle-archive';
-      }
-    }
-  }
-  return $classes;
-}
-add_filter('body_class', 'add_vehicle_archive_body_class');
-
-// Register Portfolio Custom Post Type
+/**
+ * Register Custom Post Type Portfolio
+ */
 function register_portfolio_cpt() {
-	$labels = array(
-		'name' => 'Portfolios',
-		'singular_name' => 'Portfolio',
-	);
-
 	$args = array(
 		'label' => 'Portfolio',
 		'public' => true,
+		'has_archive' => 'portfolio',
 		'rewrite' => array('slug' => 'portfolio', 'with_front' => false),
-		'has_archive' => true,
 		'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
 	);
-
 	register_post_type('portfolio', $args);
 }
 add_action('init', 'register_portfolio_cpt');
 
-// Register Custom Taxonomy Make and associate with multiple post types
+/**
+ * Register Make Taxonomy
+ */
 function register_make_taxonomy() {
-	$labels = array(
-		'name' => 'Makes',
-		'singular_name' => 'Make',
-	);
-
 	$args = array(
 		'label' => 'Make',
-		'rewrite' => array('slug' => '', 'hierarchical' => true),
+		'rewrite' => array('slug' => '', 'with_front' => false, 'hierarchical' => true),
 		'hierarchical' => true,
 	);
-
-	register_taxonomy('make', array('vehicle', 'portfolio', 'product'), $args);
+	register_taxonomy('make', array('vehicle'), $args);
 }
 add_action('init', 'register_make_taxonomy');
 
-// Видалення /make/ зі структури URL
-function remove_make_slug_from_term_link($url, $term, $taxonomy) {
+/**
+ * Rewrite URL structure for the taxonomy Make
+ */
+function make_term_link($url, $term, $taxonomy) {
 	if ($taxonomy === 'make') {
-		$url = str_replace('/make/', '/', $url);
+		$parent = get_term($term->parent, 'make');
+		if ($parent && !is_wp_error($parent)) {
+			$url = home_url("/" . $parent->slug . "/" . $term->slug . "/");
+		} else {
+			$url = home_url("/" . $term->slug . "/");
+		}
 	}
 	return $url;
 }
-add_filter('term_link', 'remove_make_slug_from_term_link', 10, 3);
+add_filter('term_link', 'make_term_link', 10, 3);
 
-// Переписані правила
-function custom_vehicle_rewrite_rules() {
-	// Архів vehicle
-	add_rewrite_rule('^vehicle/?$', 'index.php?post_type=vehicle', 'top');
-
-	// Таксономія make (без /make/)
+/**
+ * Custom rewrite rules for Vehicle and Portfolio
+ */
+function custom_rewrite_rules() {
+	// Make hierarchy
 	add_rewrite_rule('^([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[1]', 'top');
-	add_rewrite_rule('^([^/]+)/([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[2]&make_parent=$matches[1]', 'top');
+	add_rewrite_rule('^([^/]+)/([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[2]', 'top');
 
-	// Окремі vehicle пости (make/make-child/post-name)
+	// Vehicle post
 	add_rewrite_rule('^([^/]+)/([^/]+)/([^/]+)/?$', 'index.php?post_type=vehicle&name=$matches[3]', 'top');
+
+	add_rewrite_rule('^vehicle/?$', 'index.php?post_type=vehicle', 'top');
 }
-add_action('init', 'custom_vehicle_rewrite_rules');
+add_action('init', 'custom_rewrite_rules');
 
-// Фільтр на URL
+/**
+ * Custom permalink structure for Vehicle
+ */
 function vehicle_permalink_structure($post_link, $post) {
-	if ('vehicle' === get_post_type($post) && !empty($post->ID)) {
+	if ('vehicle' === get_post_type($post)) {
 		$terms = get_the_terms($post->ID, 'make');
-
 		if ($terms && !is_wp_error($terms)) {
-			$parent_term = null;
-			$child_term = null;
-
+			$hierarchy = array();
 			foreach ($terms as $term) {
-				if ($term->parent == 0) {
-					$parent_term = $term;
-				} else {
-					$child_term = $term;
-				}
+				$hierarchy[$term->term_id] = $term;
 			}
-
-			$make = $parent_term ? $parent_term->slug : 'no-make';
-			$child = $child_term ? $child_term->slug : 'no-model';
-
-			return home_url('/' . $make . '/' . $child . '/' . $post->post_name . '/');
-		} else {
-			return home_url('/no-make/no-model/' . $post->post_name . '/');
+			usort($hierarchy, function ($a, $b) {
+				return $a->parent - $b->parent;
+			});
+			$make_slug = isset($hierarchy[0]) ? $hierarchy[0]->slug : 'no-make';
+			$make_child_slug = isset($hierarchy[1]) ? $hierarchy[1]->slug : '';
+			return home_url("/{$make_slug}/" . (!empty($make_child_slug) ? "{$make_child_slug}/" : '') . "{$post->post_name}/");
 		}
 	}
 	return $post_link;
@@ -131,6 +102,13 @@ add_filter('post_type_link', 'vehicle_permalink_structure', 10, 2);
 
 
 
+/**
+ * Flush rewrite rules on activation
+ */
+function flush_rewrite_rules_on_activation() {
+	flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'flush_rewrite_rules_on_activation');
 
 
 function register_location_taxonomy() {
