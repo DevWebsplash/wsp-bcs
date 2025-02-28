edcd952b9eb57f7d9062e257fe7d3b5ad652e6a4
Dev_WSP <dev.wspro@gmail.com>
2025-02-28 19:20:11 +0200

    Vehicle _ url fix

--- bcs/functions/func-taxonomies.php
+++ bcs/functions/func-taxonomies.php
@@ -10,9 +10,12 @@ function register_vehicle_cpt() {
 	$args = array(
 		'label' => 'Vehicle',
 		'public' => true,
-		'has_archive' => 'vehicle',
+//		'has_archive' => 'vehicle',
+    'has_archive' => true,
 		'rewrite' => array('slug' => 'vehicle', 'with_front' => false),
 		'supports' => array('title', 'editor', 'custom-fields'),
+    'menu_icon' => 'dashicons-car',
+    'menu_position' => 10,
 	);
 	register_post_type('vehicle', $args);
 }
@@ -25,13 +28,37 @@ function register_portfolio_cpt() {
 	$args = array(
 		'label' => 'Portfolio',
 		'public' => true,
-		'has_archive' => 'portfolio',
+//		'has_archive' => 'portfolio',
+    'has_archive' => true,
 		'rewrite' => array('slug' => 'portfolio', 'with_front' => false),
 		'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
+    'menu_icon' => 'dashicons-portfolio',
+    'menu_position' => 10,
 	);
 	register_post_type('portfolio', $args);
 }
 add_action('init', 'register_portfolio_cpt');
+
+
+function custom_rewrite_rules() {
+  // Portfolio rules stay first (highest priority)
+  add_rewrite_rule('^portfolio/?$', 'index.php?post_type=portfolio', 'top');
+  add_rewrite_rule('^portfolio/([^/]+)/?$', 'index.php?post_type=portfolio&name=$matches[1]', 'top');
+
+  // Make hierarchy
+  add_rewrite_rule('^([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[1]', 'top');
+  add_rewrite_rule('^([^/]+)/([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[2]', 'top');
+
+  // Vehicle post
+  add_rewrite_rule('^([^/]+)/([^/]+)/([^/]+)/?$', 'index.php?post_type=vehicle&name=$matches[3]', 'top');
+
+  add_rewrite_rule('^vehicle/?$', 'index.php?post_type=vehicle', 'top');
+
+  // Location taxonomy rules
+  add_rewrite_rule('^location/([^/]+)/?$', 'index.php?taxonomy=location&term=$matches[1]', 'top');
+}
+add_action('init', 'custom_rewrite_rules');
+
 
 /**
  * Register Make Taxonomy
@@ -65,17 +92,18 @@ add_filter('term_link', 'make_term_link', 10, 3);
 /**
  * Custom rewrite rules for Vehicle and Portfolio
  */
-function custom_rewrite_rules() {
-	// Make hierarchy
-	add_rewrite_rule('^([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[1]', 'top');
-	add_rewrite_rule('^([^/]+)/([^/]+)/?$', 'index.php?taxonomy=make&term=$matches[2]', 'top');
 
-	// Vehicle post
-	add_rewrite_rule('^([^/]+)/([^/]+)/([^/]+)/?$', 'index.php?post_type=vehicle&name=$matches[3]', 'top');
 
-	add_rewrite_rule('^vehicle/?$', 'index.php?post_type=vehicle', 'top');
+function check_and_flush_rewrite_rules() {
+  // Use a transient to avoid flushing on every page load
+  if (get_transient('portfolio_rules_flushed') !== 'yes') {
+    flush_rewrite_rules();
+    set_transient('portfolio_rules_flushed', 'yes', 120);
+  }
 }
-add_action('init', 'custom_rewrite_rules');
+add_action('init', 'check_and_flush_rewrite_rules', 99);
+
+
 
 /**
  * Custom permalink structure for Vehicle
@@ -105,10 +133,91 @@ add_filter('post_type_link', 'vehicle_permalink_structure', 10, 2);
 /**
  * Flush rewrite rules on activation
  */
-function flush_rewrite_rules_on_activation() {
-	flush_rewrite_rules();
+//function flush_rewrite_rules_on_activation() {
+//	flush_rewrite_rules();
+//}
+//register_activation_hook(__FILE__, 'flush_rewrite_rules_on_activation');
+
+//function flush_rules_after_update() {
+//  flush_rewrite_rules();
+//}
+//register_activation_hook(__FILE__, 'flush_rules_after_update');
+
+
+
+function check_post_type_settings() {
+  if (current_user_can('administrator') && isset($_GET['check_cpt'])) {
+    $portfolio_settings = get_post_type_object('portfolio');
+    echo '<pre>';
+    print_r($portfolio_settings);
+    echo '</pre>';
+    exit;
+  }
 }
-register_activation_hook(__FILE__, 'flush_rewrite_rules_on_activation');
+add_action('init', 'check_post_type_settings', 999);
+
+function debug_portfolio_query() {
+  if (is_404() && strpos($_SERVER['REQUEST_URI'], '/portfolio/') !== false) {
+    global $wp_query, $wp_rewrite;
+
+    // Log requested URL and matching patterns
+    error_log('404 Portfolio URL: ' . $_SERVER['REQUEST_URI']);
+    error_log('WP Query vars: ' . print_r($wp_query->query_vars, true));
+    error_log('Post type: ' . get_query_var('post_type'));
+    error_log('Name: ' . get_query_var('name'));
+
+    // Check if post exists with that slug
+    $slug = basename(rtrim($_SERVER['REQUEST_URI'], '/'));
+    $post = get_page_by_path($slug, OBJECT, 'portfolio');
+    error_log('Post exists check: ' . ($post ? 'Yes, ID: '.$post->ID : 'No'));
+  }
+}
+add_action('wp', 'debug_portfolio_query');
+
+
+function print_matched_rewrite_rule() {
+  global $wp, $wp_rewrite;
+
+  if (isset($_GET['debug_matched_rule'])) {
+    $matched_rule = $wp->matched_rule;
+    $matched_query = $wp->matched_query;
+
+    echo '<pre>';
+    echo "Requested URL: " . $wp->request . "\n";
+    echo "Matched Rule: " . $matched_rule . "\n";
+    echo "Matched Query: " . $matched_query . "\n";
+    print_r($wp_rewrite->rules);
+    echo '</pre>';
+    exit;
+  }
+}
+add_action('wp', 'print_matched_rewrite_rule');
+
+function disable_custom_rewrites() {
+  if (isset($_GET['no_custom_rules'])) {
+    remove_all_actions('generate_rewrite_rules');
+  }
+}
+add_action('init', 'disable_custom_rewrites', 1);
+
+
+function check_portfolio_registration() {
+  add_action('init', function() {
+    error_log('Portfolio post type exists: ' . (post_type_exists('portfolio') ? 'yes' : 'no'));
+    error_log('Portfolio post type settings: ' . print_r(get_post_type_object('portfolio'), true));
+  }, 999);
+}
+check_portfolio_registration();
+
+
+
+
+
+
+
+
+
+
 
 
 function register_location_taxonomy() {
